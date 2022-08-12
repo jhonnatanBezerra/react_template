46a05c20efd7fe5725aa8271feea973a283010db
jhonnatanBezerra <jhonnatan.bezerra@estudante.ifms.edu.br>
2022-08-12 09:15:47 -0400

    add new regex mask

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -144,6 +144,25 @@ export const functions = {
 
   maskCNPJ: (value) => {
     return value.replace(/\D/g, '').replace(/(\d{2})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+  },
+
+  maskPhoneDDI: (tel) => {
+    tel = tel.replace(/\D/g, "")
+    tel = tel.replace(/^(\d)/, "+$1")
+    tel = tel.replace(/(.{3})(\d)/, "$1($2")
+    tel = tel.replace(/(.{6})(\d)/, "$1)$2")
+    if (tel.length == 12) {
+      tel = tel.replace(/(.{1})$/, "-$1")
+    } else if (tel.length == 13) {
+      tel = tel.replace(/(.{2})$/, "-$1")
+    } else if (tel.length == 14) {
+      tel = tel.replace(/(.{3})$/, "-$1")
+    } else if (tel.length == 15) {
+      tel = tel.replace(/(.{4})$/, "-$1")
+    } else if (tel.length > 15) {
+      tel = tel.replace(/(.{4})$/, "-$1")
+    }
+    return tel;
   }
 
 
